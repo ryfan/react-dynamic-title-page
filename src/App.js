af8c99090223dd52ae9432d070c111324740ce99
Ryfan Aditya Indra <ryfandotnet@gmail.com>
2020-09-21 08:28:12 +0700

    Dynamic Title Page with React Helmet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <Helmet>
+        <title>Title with React Helmet</title>
+        <meta name="description" content="Dynamic title page with React Helmet" />
+      </Helmet>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            dynamic title page with <code>React Helmet</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </>
   );
 }
 
